023a7640aed0e9d6b83547c2f3c9c67858c4c9c1
Ssousuke <wesleysousukee@gmail.com>
2022-04-27 23:16:45 -0300

    Modificadores de acesso Ref e Out

---
+++ ModificadoresRefOut/Program.cs
@@ -0,0 +1,38 @@
+﻿// Modificadores de acesso Ref e Out
+namespace ModificaroesRefOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            Calculator.Triple(ref x);
+            Console.WriteLine($"Triple: {x}");
+
+
+            int z = 10;
+            int quadruple;
+            Calculator.Quadruple(z, out quadruple);
+            Console.WriteLine($"Quadruple: {quadruple}");
+        }
+    }
+
+    class Calculator
+    {
+        // Ref
+        // A variável passada como parametro Ref deve ser iniciada
+        public static void Triple(ref int value)
+        {
+            value *= 3;
+        }
+
+        // Out
+        // A variável passada como parametro Out não precisa ter sido iniciada
+        public static void Quadruple(int origin, out int result)
+        {
+            result = origin * 4;
+        }
+    }
+}
+
+
