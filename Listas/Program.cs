f125161cd72a081a594242e58e76048ac04620e5
Wesley Farias <wesleysousukee@gmail.com>
2022-04-28 16:53:38 -0300

    listas pt1

---
+++ Listas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciando uma lista
+            List<string> list = new List<string>();
+
+            // Adicionando dados de forma sequencial
+            list.Add("Mariano");
+            list.Add("Maria");
+            list.Add("Marcos");
+            list.Add("Matheus");
+            list.Add("Wesley");
+            list.Add("João");
+            list.Add("Lucas");
+
+            // Inserindo
+            // A função Insert recebe a posição e o valor a ser inserido
+            list.Insert(0, "Ana");
+            list.Insert(7, "Zeca");
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item} ");
+            }
+            Console.WriteLine($"Total: {list.Count}");
+
+            // Buscar a primeira pessoa por letra
+            string first = list.Find(x => x[0] == 'M');
+            Console.WriteLine($"Primeiro nome com a letra A: {first}");
+        }
+    }
+}
