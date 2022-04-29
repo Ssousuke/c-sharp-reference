68a13455244feb1be3abdf64f8c830a850984b84
Ssousuke <wesleysousukee@gmail.com>
2022-04-29 00:24:13 -0300

    listaspt2

--- Listas/Program.cs
+++ Listas/Program.cs
@@ -31,7 +31,35 @@ namespace Listas
 
             // Buscar a primeira pessoa por letra
             string first = list.Find(x => x[0] == 'M');
-            Console.WriteLine($"Primeiro nome com a letra A: {first}");
+            Console.WriteLine($"Primeiro nome com a letra M: {first}");
+
+            // Bucas a última pessoa por letra
+            string last = list.FindLast(x => x[0] == 'M');
+            Console.WriteLine($"Último nome com a letra M: {last}");
+
+            // Buscando a posição do primeiro item que começa com a letra
+            int pos1 = list.FindIndex(x => x[0] == 'M');
+            Console.WriteLine($"Primeira posição com a letra 'M': {pos1}");
+
+            // Buscando a última posição do item que começa com a letra
+            int pos2 = list.FindLastIndex(x => x[0] == 'M');
+            Console.WriteLine($"Última posição com a letra 'M': {pos2}");
+
+            // Removendo o item pelo valor
+            list.Remove("Zeca");
+            Console.WriteLine("Remove Zeca:");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Remove todos os itens que começam com a letra
+            Console.WriteLine("Remove todos os nomes que começam com M: ");
+            list.RemoveAll(x => x[0] == 'M');
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
