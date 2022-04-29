0e20d7d6180ec5cccf1249c379d09997c350dee0
Ssousuke <wesleysousukee@gmail.com>
2022-04-29 02:23:37 -0300

    Resolução do exercicio de fixação pt1

---
+++ ExercicioFixacaoListas/Employee/Employee.cs
@@ -0,0 +1,21 @@
+namespace ExercicioFixacaoListas
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Name}, {Salary}";
+        }
+    }
+}
---
+++ ExercicioFixacaoListas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ExercicioFixacaoListas
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employe = new List<Employee>();
+
+            int quantity = 3;
+
+            for (int i = 0; i < quantity; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                string name = Console.ReadLine();
+                double salary = double.Parse(Console.ReadLine());
+
+                employe.Add(new Employee(id, name, salary));
+            }
+
+            foreach (var item in employe)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
