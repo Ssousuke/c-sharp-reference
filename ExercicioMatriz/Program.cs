148e513190cb89aefbfb0129c2d30c145f22b0d2
Ssousuke <wesleysousukee@gmail.com>
2022-05-01 15:47:11 -0300

    matrizes

---
+++ ExercicioMatriz/Program.cs
@@ -0,0 +1,45 @@
+﻿// Fazer um programa para ler um número inteiro N e uma matriz de ordem N contendo
+// números inteiros. Em seguida, mostrar a diagonal principal e a quantidade de valores negativos da matriz
+using System;
+namespace ExercicioMatriz
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] mat = new int[n, n];
+
+            // Percorre as linhas
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                // Percorre as colunas
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            Console.Write("Diagonal Principal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{mat[i, i]} ");
+            }
+
+            Console.WriteLine();
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Números negativos: {count}");
+        }
+    }
+}
---
+++ MatrizesIntroducao/Matriz.cs
@@ -0,0 +1,18 @@
+﻿// Instanciando uma Matriz
+class Matriz
+{
+    static void Main(string[] args)
+    {
+        // Instanciando uma matriz de duas linhas e três colunas
+        double[,] matriz = new double[2, 3];
+
+        // Lenght é usando para mostrar o total de elementos na matriz
+        Console.WriteLine(matriz.Length);
+
+        // GetRank é usado para mostrar o número de demenssões da matriz
+        Console.WriteLine(matriz.Rank);
+
+        // GetLenght mostra a quantidade de dimenssões de um elemento especifico no Array
+        Console.WriteLine(matriz.GetLength(0));
+    }
+}
