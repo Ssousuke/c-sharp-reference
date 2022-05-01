8677c54bb774dc8bb2d7219dbf2ffb45525c6ebe
Ssousuke <wesleysousukee@gmail.com>
2022-05-01 20:37:42 -0300

    Topicos Especiais PT1

---
+++ TopicosEspeciaisPT1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace TopicosEspeciaisPT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inferência de Tipos: Palavra var
+            // Os tipos são inferidos em tempo de compilação e pelo proprio compilador
+            // int
+            var x = 10;
+            // double 
+            var y = 20.0;
+            // string 
+            var z = "Olá Mundo!";
+            Console.WriteLine($"Tipo: {x.GetType()}");
+            Console.WriteLine($"Tipo: {y.GetType()}");
+            Console.WriteLine($"Tipo: {z.GetType()}");
+
+            // Data e Hora atual
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+
+        }
+    }
+}
